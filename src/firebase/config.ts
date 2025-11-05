90052ca5bab9ea0d75ee5e5031315435a8a8671c
Innovacion Municipalidad Tunuyan <tunuyanchatbot@gmail.com>
2025-11-05 14:43:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7184675653-a4cc6",
+  "appId": "1:496821863464:web:038e5c71a4ada14d45e11d",
+  "apiKey": "AIzaSyDvrE_erSy65SZaonL4M9QvwpZ3S-eA7e8",
+  "authDomain": "studio-7184675653-a4cc6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "496821863464"
+};
