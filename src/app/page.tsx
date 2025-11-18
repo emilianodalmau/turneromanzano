a24c06472dac91a465071814f06275012251e89f
Innovacion Municipalidad Tunuyan <tunuyanchatbot@gmail.com>
2025-11-18 12:57:26 +0000

    elimina este tirulo Bienvenidos al gestor de turnos del Museo del Manzan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,6 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <div className="text-center space-y-4">
-        <h1 className="text-4xl font-bold text-foreground">
-          Bienvenidos al gestor de turnos del Museo del Manzano Historico
-        </h1>
         <div className="flex justify-center gap-4 pt-4">
           <Link href="/login" passHref>
             <Button>Iniciar Sesión</Button>
