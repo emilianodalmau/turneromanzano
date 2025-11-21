e4be8827a875590b16a3d989a6593571fe26106a
Innovacion Municipalidad Tunuyan <tunuyanchatbot@gmail.com>
2025-11-21 15:39:45 +0000

    tudio-7184675653:~/studio{main}$ gsutil cors set cors.json gs://studio-7

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7184675653-a4cc6",
   "appId": "1:496821863464:web:038e5c71a4ada14d45e11d",
-  "storageBucket": "studio-7184675653-a4cc6.appspot.com",
+  "storageBucket": "studio-7184675653-a4cc6.firebasestorage.app",
   "apiKey": "AIzaSyDvrE_erSy65SZaonL4M9QvwpZ3S-eA7e8",
   "authDomain": "studio-7184675653-a4cc6.firebaseapp.com",
   "measurementId": "",
